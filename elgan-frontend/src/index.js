1ba6bcc07af719175f4aaed4ccd49acf240e1c5e
Omotoso Mojuba <52413017+mojuba11@users.noreply.github.com>
2026-04-19 11:13:50 +0100

    Update index.js

--- elgan-frontend/src/index.js
+++ elgan-frontend/src/index.js
@@ -1,3 +1,9 @@
+/**
+ * ELGAN INTEGRATED OFFSHORE WASTE MANAGEMENT SYSTEM
+ * Short Name: Elgan Integrated
+ * Version: 2.0.0
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
